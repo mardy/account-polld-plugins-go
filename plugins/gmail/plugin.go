dce3c0cfc63c86b1f760d9a9b434ff501f6640b1
Sergio Schvezov <sergio.schvezov@canonical.com>
2014-07-08 22:01:44 -0300

    Initial gmail plugin

---
+++ plugins/gmail/plugin.go
@@ -0,0 +1,44 @@
+/*
+ Copyright 2014 Canonical Ltd.
+ Authors: Sergio Schvezov <sergio.schvezov@canonical.com>
+
+ This program is free software: you can redistribute it and/or modify it
+ under the terms of the GNU General Public License version 3, as published
+ by the Free Software Foundation.
+
+ This program is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY; without even the implied warranties of
+ MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+ PURPOSE.  See the GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along
+ with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package gmail
+
+import "launchpad.net/account-polld/plugins"
+
+const APP_ID = "com.ubuntu.developer.webapps.webapp-gmail"
+
+type GmailPlugin struct {
+	state      plugins.State
+	stateWatch chan (plugins.State)
+}
+
+func New() *GmailPlugin {
+	return &GmailPlugin{}
+}
+
+func (p *GmailPlugin) Poll() (*[]plugins.Notification, error) {
+	return nil, nil
+}
+
+func (p *GmailPlugin) Register() (plugins.ApplicationId, chan (plugins.State)) {
+	p.stateWatch = make(chan plugins.State)
+	return plugins.ApplicationId(APP_ID), p.stateWatch
+}
+
+func (p *GmailPlugin) Unregister() {
+	close(p.stateWatch)
+}
