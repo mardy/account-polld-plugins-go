5030f389ba68a58890ce8b78986e479d9571f2f1
Niklas Wenzel <nikwen.developer@gmail.com>
2014-10-20 16:35:57 +0200

    Fix authors

--- cmd/account-polld/account_manager.go
+++ cmd/account-polld/account_manager.go
@@ -1,6 +1,7 @@
 /*
  Copyright 2014 Canonical Ltd.
  Authors: Sergio Schvezov <sergio.schvezov@canonical.com>
+ Authors: Niklas Wenzel <nikwen.developer@gmail.com>
 
  This program is free software: you can redistribute it and/or modify it
  under the terms of the GNU General Public License version 3, as published
