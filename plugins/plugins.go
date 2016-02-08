44d5fee98f0034b002fc7b244fa4a38a6dee5fae
Niklas Wenzel <nikwen.developer@gmail.com>
2016-02-08 20:19:46 +0100

    Pick 2

--- plugins/plugins.go
+++ plugins/plugins.go
@@ -68,7 +68,7 @@ func NewStandardPushMessage(summary, body, action, icon string, epoch int64) *Pu
 				Persist:   true,
 			},
 			Sound:   DefaultSound(),
-			Vibrate: DefaultVibration(),
+			Vibrate: true,
 			Tag:     cmdName,
 		},
 	}
@@ -249,9 +249,4 @@ func FromPersist(pluginName string, accountId uint, data interface{}) (err error
 func DefaultSound() string {
 	// path is searched within XDG_DATA_DIRS
 	return "sounds/ubuntu/notifications/Blip.ogg"
-}
-
-// DefaultVibration returns a Vibrate with the default vibration
-func DefaultVibration() *Vibrate {
-	return &Vibrate{Duration: 1000}
 }
